c8b84f9f483e29f23dead09314b6c0b0ca499e60
Souhayb007 <162489946+Souhayb007@users.noreply.github.com>
2024-08-17 21:37:05 +0200

    first commit projet de chat

---
+++ src/main/java/com/alibou/chat/WebSocketEventListener.java
@@ -0,0 +1,38 @@
+package com.alibou.chat;
+
+import com.alibou.chat.chat.ChatMessage;
+import com.alibou.chat.chat.MessageType;
+import jakarta.websocket.Session;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void HandleWebSocketDisconnetListener(
+            SessionDisconnectEvent event
+    )
+    {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username =(String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("User disconnected;{} ",username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+        }
+    }
+}
